5cd45baa8db5ca757be7fd9e0bc5188b5dc04730
ib-rim <ib.bashir@hotmail.com>
2023-06-05 17:12:03 +0100

    Add read and delete operations

---
+++ SpringAPI/src/main/java/com/example/SpringAPI/api/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.example.SpringAPI.api.controller;
+
+import com.example.SpringAPI.api.model.User;
+import com.example.SpringAPI.service.UserService;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/user")
+    public User getUser(@RequestParam Integer id) {
+        return userService.getUser(id);
+    }
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return userService.getUsers();
+    }
+
+    @DeleteMapping("/user")
+    public User deleteUser(@RequestParam Integer id) {
+        return userService.deleteUser(id);
+    }
+}
---
+++ SpringAPI/src/main/java/com/example/SpringAPI/service/UserService.java
@@ -0,0 +1,47 @@
+package com.example.SpringAPI.service;
+
+import com.example.SpringAPI.api.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private List<User> userList;
+
+    public UserService() {
+        userList = new ArrayList<>();
+
+        User user1 = new User(1, "Ibrahim", 21, "ib.bashir@hotmail.com");
+        User user2 = new User(2, "Atikah", 22, "a@hotmail.com");
+
+        userList.addAll(Arrays.asList(user1, user2));
+    }
+    public User getUser(Integer id) {
+        for (User user: userList) {
+            if(id == user.getId()) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public List<User> getUsers() {
+        return userList;
+    }
+
+    public User deleteUser(Integer id) {
+        for (User user: userList) {
+            if(id == user.getId()) {
+                userList.remove(user);
+                return user;
+            }
+        }
+        return null;
+    }
+
+}
